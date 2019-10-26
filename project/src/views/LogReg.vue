962421d617a72500e5721f1db3f40ce927a1b90d
lzy563528837 <13398477277@163.com>
2019-10-26 09:21:54 +0800

    登陆注册页面的路由

---
+++ project/src/views/LogReg.vue
@@ -0,0 +1,76 @@
+<template>
+	<div>
+	<top-mune></top-mune>
+	<div class="logBox">
+		<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+			<el-form-item label="用户名" prop="name"><el-input v-model="ruleForm.name"></el-input></el-form-item>
+			<el-form-item label="密码" prop="pwd"><el-input type='password' v-model="ruleForm.pwd"></el-input></el-form-item>
+			<el-form-item size="large">
+				<el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
+				<el-button @click="goToRegister">注册</el-button>
+			</el-form-item>
+		</el-form>
+	</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			ruleForm: {
+				name: '',
+				pwd: ''
+			},
+			rules: {
+				name: [{ message: '请输入用户名称', trigger: 'blur' }, { min: 3, max: 8, message: '用户名称长度在 3 到 8 个字符', trigger: 'blur' }],
+				pwd: [{ message: '请输入密码', trigger: 'blur' }, { min: 6, max: 16, message: '长度在 6 到 16 个字符', trigger: 'blur' }]
+			}
+		};
+	},
+	methods: {
+		submitForm(formName) {
+			const that = this;
+			this.$refs[formName].validate(valid => {
+				if (valid) {
+					this.axios.post('/login', {
+					    username: this.ruleForm.name,
+					    pwd: this.ruleForm.pwd
+					  })
+					  .then(function (response) {
+					    console.log(response);
+						alert("登陆成功")
+						console.log(that)
+						that.$router.push('/')
+					  })
+					  .catch(function (error) {
+					    console.log(error);
+					  });
+
+					// alert('submit!');
+				} else {
+					console.log('error submit!!');
+					return false;
+				}
+			});
+		},
+		goToRegister: function() {
+			this.$router.push('/LogReg/register')
+		},
+		
+	}
+};
+</script>
+
+<style>
+.logBox {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 50%;
+	height: 600px;
+	margin: 0 auto;
+	margin-top: 40px;
+	border: 2px solid rgba(96, 255, 244, 0.2);
+}
+</style>
---
+++ project/src/views/register.vue
@@ -0,0 +1,87 @@
+<template>
+	<div>
+		<top-mune></top-mune>
+		<div class="logBox">
+			<el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+				<el-form-item label="用户名" prop="username"><el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input></el-form-item>
+				<el-form-item label="密码" prop="pass"><el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input></el-form-item>
+				<el-form-item label="确认密码" prop="checkPass"><el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input></el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+					<el-button @click="resetForm('ruleForm')">重置</el-button>
+				</el-form-item>
+			</el-form>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		var validateUsername = (rule, value, callback) => {
+			if (value === '') {
+				callback(new Error('请输入用户名'));
+			} else {
+				if (/[ \\\ / * ? : "<> | ]/.test(value)) {
+					callback(new Error('用户名含有非法字符'));
+				}
+				callback();
+			}
+		};
+		var validatePass = (rule, value, callback) => {
+			if (value === '') {
+				callback(new Error('请输入密码'));
+			} else {
+				if (/[A-Za-z].*[0-9]|[0-9].*[A-Za-z]/.test(value)) {
+					this.$refs.ruleForm.validateField('checkPass');
+				} else {
+					callback(new Error('密码中必须含有数字和字母!'));
+				}
+				callback();
+			}
+		};
+		var validatePass2 = (rule, value, callback) => {
+			if (value === '') {
+				callback(new Error('请再次输入密码'));
+			} else if (value !== this.ruleForm.pass) {
+				callback(new Error('两次输入密码不一致!'));
+			} else {
+				callback();
+			}
+		};
+		return {
+			ruleForm: {
+				username: '',
+				pass: '',
+				checkPass: ''
+			},
+			rules: {
+				username: [{ validator: validateUsername, trigger: 'blur' }],
+				pass: [{ validator: validatePass, trigger: 'blur' }],
+				checkPass: [{ validator: validatePass2, trigger: 'blur' }]
+			}
+		};
+	},
+	methods: {
+		submitForm(formName) {
+			this.$refs[formName].validate(valid => {
+				if (valid) {
+					alert('submit!');
+				} else {
+					console.log('error submit!!');
+					return false;
+				}
+			});
+		},
+		resetForm(formName) {
+			this.$refs[formName].resetFields();
+		}
+	}
+};
+</script>
+
+<style scoped="">
+.el-form-item {
+	margin-bottom: 32px;
+}
+</style>
